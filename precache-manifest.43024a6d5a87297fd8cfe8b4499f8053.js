5208ea634311d02f5a73afad322912967b0b618f
ioni <ioni_dev@protonmail.com>
2019-07-09 20:43:45 -0300

    Updates

---
+++ precache-manifest.43024a6d5a87297fd8cfe8b4499f8053.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e18dfd8222f1b3fefa5a52d438a435e4",
+    "url": "/SpotiSearch/index.html"
+  },
+  {
+    "revision": "fcb20c769a7f0746d337",
+    "url": "/SpotiSearch/static/css/main.0509c6d6.chunk.css"
+  },
+  {
+    "revision": "8be47e9d49b2693d858b",
+    "url": "/SpotiSearch/static/js/2.e1780bb9.chunk.js"
+  },
+  {
+    "revision": "fcb20c769a7f0746d337",
+    "url": "/SpotiSearch/static/js/main.b4e175c8.chunk.js"
+  },
+  {
+    "revision": "9ac6e42083ccd56db542",
+    "url": "/SpotiSearch/static/js/runtime~main.7e98621c.js"
+  }
+]);
